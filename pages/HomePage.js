bf154dd3b219e0b619f086cbe9b8acc7cbd89ec6
ferreiracamilo <camiloferreira2009@hotmail.com>
2025-03-30 12:09:42 -0300

    add new methods and erase example methods

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -10,23 +10,18 @@ class HomePage{
         this.#tabCloseBtns = page.locator(homePageLocators.tabCloseBtns);
     }
 
-    /**
-     * Custom Wait - Verify main elements from home page are present
-     * * This method checks if the following elements are visible:
-     * - User Profile Icon
-     * - Notifications Icon
-     * - Help Icon
-     * - Guidance Center Icon
-     * - Favorite Icon
-     *
-    */
-    async verifyHomePageIsLoaded(){
-        await test.step(`Verify main home page elements are present`, async () => {
-            //await expect(this.#userProfileIcon).toBeVisible();
-            //await expect(this.#notificationsIcon).toBeVisible();
-            //await expect(this.#helpIcon).toBeVisible();
-            //await expect(this.#guidanceCenterIcon).toBeVisible();
-            //await expect(this.#favoriteIcon).toBeVisible();
+    async closeWelcomeModal(){
+        await this.#page.waitForTimeout(4000);
+        if(this.#page.getByRole('button', { name: 'Thank U, next!' }).isVisible()){
+            await this.#page.getByRole('button', { name: 'Thank U, next!' }).click();
+        }
+    }
+
+    async searchProducts(keyword){
+        await test.step(`Search products '${keyword}'`, async () => {
+            await this.#page.getByPlaceholder('Buscar productos...').click();
+            await this.#page.getByPlaceholder('Buscar productos...').fill('keyword');
+            await this.#page.getByPlaceholder('Buscar productos...').press('Enter');
         });
     }
 }
